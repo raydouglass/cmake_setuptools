3d7a0d85aea75a18f692a14fbb5e754acaa00b23
Raymond Douglass <ray@raydouglass.com>
2019-01-18 13:08:21 -0500

    BLD Setup.py updates

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ with open('README.md', encoding='utf-8') as f:
 setup(name='cmake_setuptools',
       description='Provides some usable cmake related build extensions',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Ray Douglass',
       url='https://github.com/raydouglass/cmake_setuptools',
       version=__version__,
       install_requires=['setuptools'],
       license="Apache 2.0",
-      packages=find_packages())
+      packages=find_packages(),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Build Tools'
+      ])
